db5262f4290a58d85eb0e9f85973e34c21dfae22
wanglu3265 <wanglu3265@706.com>
2023-07-21 11:07:02 +0800

    Initial commit

---
+++ src/views/ModelMarket/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: wanglu
+ * @Date: 2023-07-20 14:52:21
+ * @LastEditors: wanglu
+ * @LastEditTime: 2023-07-20 17:25:31
+ * @Description: 
+-->
+<template>
+  <div class="">
+模型市场   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+
+</style>
---
+++ src/views/modelManagement/soldOut/index.vue
@@ -0,0 +1,43 @@
+<!--
+ * @Author: wanglu
+ * @Date: 2023-07-20 14:52:21
+ * @LastEditors: wanglu
+ * @LastEditTime: 2023-07-20 18:06:49
+ * @Description: 
+-->
+<template>
+  <div class="">
+模型下架    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+
+</style>
