5be7dd3e47be6c138add30e85536b365f717a099
wanglu3265 <wanglu3265@706.com>
2024-06-13 14:05:15 +0800

    fix: 后台地址变更

--- src/views/ModelMarket/component/Card.vue
+++ src/views/ModelMarket/component/Card.vue
@@ -1,8 +1,8 @@
 <!--
  * @Author: wanglu
  * @Date: 2023-07-24 19:29:18
- * @LastEditors: wanglu
- * @LastEditTime: 2023-09-19 14:33:24
+ * @LastEditors: Wang Lu
+ * @LastEditTime: 2024-06-03 15:45:55
  * @Description:
 -->
 <template>
@@ -26,10 +26,10 @@
       </span>
 
       <p class="box-card__text--provider mt-2 mb-1">
-        {{ card.address || '北京市丰台区人民检察院' }}
+        {{ card.address || "XXX检察院" }}
       </p>
     </div>
-    <div class="box-card__operator flex justify-between  items-center mt-4">
+    <div class="box-card__operator flex justify-between items-center mt-4">
       <div class="box-card__operator--icon">
         <el-tooltip
           class="box-item"
@@ -37,7 +37,14 @@
           content="订阅次数"
           placement="top"
         >
-          <img class="box-card__operator--iconNum-icon" :src="card.subscribeStatus === 1 ? '/images/lh/orderLightIcon.svg' : '/images/lh/orderIcon.svg'">
+          <img
+            class="box-card__operator--iconNum-icon"
+            :src="
+              card.subscribeStatus === 1
+                ? '/images/lh/orderLightIcon.svg'
+                : '/images/lh/orderIcon.svg'
+            "
+          />
         </el-tooltip>
         <div class="box-card__operator--iconNum-num">
           {{ card.subscribeNumber }}
@@ -50,7 +57,14 @@
           content="收藏次数"
           placement="top"
         >
-          <img class="box-card__operator--iconNum-icon" :src="card.collectStatus === 1 ? '/images/lh/starLightIcon.svg' : '/images/lh/starIcon.svg'">
+          <img
+            class="box-card__operator--iconNum-icon"
+            :src="
+              card.collectStatus === 1
+                ? '/images/lh/starLightIcon.svg'
+                : '/images/lh/starIcon.svg'
+            "
+          />
         </el-tooltip>
         <div class="box-card__operator--iconNum-num">
           {{ card.collectNumber }}
@@ -63,7 +77,11 @@
           content="添加购物车"
           placement="top"
         >
-          <i class="iconfont icon-maijiagouwuche color-blue font-size-3" style="transform: scale(1.5);" @click="addShopping" />
+          <i
+            class="iconfont icon-maijiagouwuche color-blue font-size-3"
+            style="transform: scale(1.5)"
+            @click="addShopping"
+          />
         </el-tooltip>
         <!-- <div v class="box-card__operator--iconNum-num">
           {{ card.publishDate }}
@@ -74,37 +92,38 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
-import { ElMessage } from 'element-plus'
+import { computed } from "vue";
+import { ElMessage } from "element-plus";
 
 interface CardItem {
-  id: number
-  productImg: string
-  title: string
-  describe: string
-  label: string
-  address: string
-  subscribeNumber: number
-  collectNumber: number
-  publishDate: string
-  subscribeStatus: number
-  collectStatus: number
+  id: number;
+  productImg: string;
+  title: string;
+  describe: string;
+  label: string;
+  address: string;
+  subscribeNumber: number;
+  collectNumber: number;
+  publishDate: string;
+  subscribeStatus: number;
+  collectStatus: number;
 }
-const baseUrl = import.meta.env.VITE_APP_BASE_API
 const props = defineProps<{
-  card: CardItem
-}>()
+  card: CardItem;
+}>();
+const baseUrl = import.meta.env.VITE_APP_BASE_API;
 function addShopping() {
   ElMessage({
-    message: '购物车添加成功',
-    type: 'success',
-  })
+    message: "购物车添加成功",
+    type: "success",
+  });
 }
-const str = '梳理本院受理的未成年人涉黄类案件发现，未成年人涉黄类案件中大多存在未成年人在宾馆、日租房内进行卖淫嫖娼活动或在KTV内进行有偿陪侍的情形。经审查发现，上述日租房、宾馆均存在未按规定落实未成年人入住询问、登记等制度，KTV等场所也未落实不允许未成年人进入的相关规定，严重侵害了未成年人的合法权益，同时也暴露出相关行政主管机关存在怠于履行监管职责的情形。\n通过筛查全国检察业务应用系统2.0涉黄类案件数据以及北京市公安局丰台分局治安违法案件数据发现宾馆、日租房、酒店等住宿经营者可能存在未落实未成年人入住询问、登记制度问题的违法线索。'
+const str =
+  "梳理本院受理的未成年人涉黄类案件发现，未成年人涉黄类案件中大多存在未成年人在宾馆、日租房内进行卖淫嫖娼活动或在KTV内进行有偿陪侍的情形。经审查发现，上述日租房、宾馆均存在未按规定落实未成年人入住询问、登记等制度，KTV等场所也未落实不允许未成年人进入的相关规定，严重侵害了未成年人的合法权益，同时也暴露出相关行政主管机关存在怠于履行监管职责的情形。\n通过筛查全国检察业务应用系统2.0涉黄类案件数据以及北京市公安局丰台分局治安违法案件数据发现宾馆、日租房、酒店等住宿经营者可能存在未落实未成年人入住询问、登记制度问题的违法线索。";
 
 const introducesData = computed(() => {
-  return props.card.introduces
-})
+  return props.card.introduces;
+});
 </script>
 
 <style lang="scss" scoped>
@@ -121,7 +140,7 @@ const introducesData = computed(() => {
       transition: all 0.2s linear;
     }
 
-    &:hover>&--image {
+    &:hover > &--image {
       transform: scale(1.1);
       filter: contrast(120%);
     }
@@ -151,7 +170,7 @@ const introducesData = computed(() => {
       -webkit-line-clamp: 3; //这个属性需要同上面两个属性一起使用，表示显示的行数。
     }
 
-    &--label{
+    &--label {
       background-color: #e8e8ea;
       font-size: 12px;
       color: #02091f;
@@ -165,20 +184,20 @@ const introducesData = computed(() => {
       line-height: 21px;
     }
   }
-  &__operator{
+  &__operator {
     font-size: 16px;
     color: #7b89b7;
-    &--icon{
+    &--icon {
       display: flex;
       justify-content: left;
-      align-items: center
+      align-items: center;
     }
-    &--iconNum-icon{
+    &--iconNum-icon {
       width: 16px;
       height: 16px;
       cursor: pointer;
     }
-    &--iconNum-num{
+    &--iconNum-num {
       margin-left: 4px;
       height: 16px;
       line-height: 16px;
@@ -186,9 +205,8 @@ const introducesData = computed(() => {
       font-family: Source Han Sans CN;
       font-weight: 400;
       color: #666666;
-      opacity: 0.66
+      opacity: 0.66;
     }
-
   }
 }
 </style>
